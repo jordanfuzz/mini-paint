f9afb105baf902b5a3ea89488cb6804ffb7298ec
Jordan Cooper <jordan@cooperplanet.com>
2017-06-08 13:01:28 -0600

    Enable clicking and color choices

---
+++ app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    var box = $('.box')
+    var reset = $('#reset')
+    var selectedColor = 'white'
+    var red = $('#red')
+    var green = $('#green')
+    var blue = $('#blue')
+    var yellow = $('#yellow')
+    var white = $('#white')
+
+    box.on('click', function() {
+
+        $(this).addClass(selectedColor)
+
+
+    })
+    
+    box.on('dblclick', function() {
+        
+        $(this).removeClass(selectedColor)
+    })
+
+    reset.on('click', function() {
+        box.removeClass(selectedColor)
+
+
+    })
+
+    red.on('click', function() {
+        selectedColor = 'red'
+    })
+    green.on('click', function() {
+        selectedColor = 'green'
+    })
+    blue.on('click', function() {
+        selectedColor = 'blue'
+    })
+    yellow.on('click', function() {
+        selectedColor = 'yellow'
+    })
+    white.on('click', function() {
+        selectedColor = 'white'
+    })
+
+
+})
